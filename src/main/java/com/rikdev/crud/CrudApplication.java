748a9efcbf4a6ff1013f388af5e3e991ffbb92be
richen1995 <50597352+richen1995@users.noreply.github.com>
2025-01-30 09:23:26 -0500

    cambio para que corra elproyecto en heroku

--- src/main/java/com/rikdev/crud/CrudApplication.java
+++ src/main/java/com/rikdev/crud/CrudApplication.java
@@ -2,9 +2,10 @@ package com.rikdev.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudApplication {
+public class CrudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
